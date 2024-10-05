cd3f9dfbfe5a8eeb9d2caea5f6b2878bd1974154
Satyam-Nayak <satyamn120@gmail.com>
2024-10-05 09:31:42 +0530

    ecommerce_website_satyam 1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+
+const App = () => {
+  return (  
+    <div className="App">
+      <h1>Online Purchase</h1>
+    </div>
+  );
+}
+// SDLC
+ 
+export default App;
