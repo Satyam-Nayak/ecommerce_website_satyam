cd850a1b11708936e0dae0399d64d33ec6311215
Satyam-Nayak <satyamn120@gmail.com>
2024-10-09 09:59:18 +0530

    ecommerce_website_satyam 3rd

--- src/Components/AdminLogin.jsx
+++ src/Components/AdminLogin.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import '../styles/AdminLogin.css'
 export default function AdminLogin() {
+  let [username,setUsername] = useState("");
+  let [setpassword,setPassword] = useState("");
+  console.log(username);
+  console.log(setpassword);
+
+  // password set function
+function login(){
+  if(username === "admin" && setpassword === "admin"){
+    alert("Admin Login Successfull");
+}
+else{
+  alert("Invalid Admin");
+  }
+}
+
+
   return (
-    <div>
-      
+    <div className='AdminLogin'>
+      <form action="">
+        <label htmlFor="">
+          Username :
+        </label>
+        <input type="text" value={username} onChange={(e)=>{setUsername(e.target.value)}} placeholder='Enter the User Name'  required/>
+        <label htmlFor="">
+          Password :
+        </label>
+        <input type="password" value={setpassword} onChange={(e)=>{setPassword(e.target.value)}} placeholder='Enter the password' required/>
+        <button onClick={login}>Login</button>
+      </form>
     </div>
   )
 }
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";  
-
+import '../styles/Landing.css'
 const Landing = () => {
     return ( 
         <div className="page">
             <Link to='/adminlogin'>
+            <img src="https://w7.pngwing.com/pngs/306/70/png-transparent-computer-icons-management-admin-silhouette-black-and-white-neck-thumbnail.png" alt="" />
             <h1>Admin Login</h1>
             </Link>
             <Link to='/userlogin'>
+            <img src="https://png.pngtree.com/png-clipart/20191120/original/pngtree-outline-user-icon-png-image_5045523.jpg" alt="" />
             <h1>User Login</h1>
             </Link>
         </div>
