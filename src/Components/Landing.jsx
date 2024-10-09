040eb348390a6a538a7ec3020b84dc66c4eb74d8
Satyam-Nayak <satyamn120@gmail.com>
2024-10-09 17:02:46 +0530

    Ecommerce changes 5

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -4,11 +4,11 @@ const Landing = () => {
     return ( 
         <div className="page">
             <Link to='/adminlogin'>
-            <img src="https://w7.pngwing.com/pngs/306/70/png-transparent-computer-icons-management-admin-silhouette-black-and-white-neck-thumbnail.png" alt="" />
+            <img src="https://w7.pngwing.com/pngs/306/70/png-transparent-computer-icons-management-admin-silhouette-black-and-white-neck-thumbnail.png" alt="Admin-Login" />
             <h1>Admin Login</h1>
             </Link>
             <Link to='/userlogin'>
-            <img src="https://png.pngtree.com/png-clipart/20191120/original/pngtree-outline-user-icon-png-image_5045523.jpg" alt="" />
+            <img src="https://png.pngtree.com/png-clipart/20191120/original/pngtree-outline-user-icon-png-image_5045523.jpg" alt="User-Login" />
             <h1>User Login</h1>
             </Link>
         </div>
