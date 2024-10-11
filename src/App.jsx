52142cf5bf6f6f8bd6e62578bb1a8c8017aa118e
Satyam-Nayak <satyamn120@gmail.com>
2024-10-11 09:56:25 +0530

    add back ground to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-// import './App.css';
+import './App.css';
 import React from "react";
 import Landing from "./Components/Landing";
 import { BrowserRouter,Routes,Route } from "react-router-dom";
